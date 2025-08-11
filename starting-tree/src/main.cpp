3dd589744e773da8fdd26c3b9cfe1d7a0b593b49
carlyle-felix <carlyle.felix@gmail.com>
2025-08-11 12:33:31 +0200

    starting-tree: add comments

--- starting-tree/src/main.cpp
+++ starting-tree/src/main.cpp
@@ -5,16 +5,18 @@
 #define NUM_FLASHES 3
 #define STAGE_TIME 2000
 #define MAX_REACT 3000
+#define RAND_DELAY_MIN 500
+#define RAND_DELAY_MAX 700
 
 void set_tree_state(uint8_t state);
 void reset_tree(void);
-uint8_t start_tree(uint16_t ms);
+int8_t start_tree(uint16_t ms);
 
-const uint8_t sensor = 7;
-const uint8_t s = 5;
-const uint8_t y = 4;
-const uint8_t g = 3;
-const uint8_t r = 2;
+const uint8_t sensor = 7;   // IR OBSTACLE AVOIDANCE SENSOR (SUNFOUNDER ST0050).
+const uint8_t s = 5;        // YELLOW LED.
+const uint8_t y = 4;        // YELLOW LED.
+const uint8_t g = 3;        // GREEN LED.
+const uint8_t r = 2;        // RED LED.
 
 void setup() 
 {
@@ -29,17 +31,18 @@ void setup()
 
 void loop() 
 {
-  uint8_t sensor_state, result;
+  uint8_t sensor_state;
+  int8_t result;
   uint16_t ms;
 
   set_tree_state(OFF);
 
   srand(analogRead(A0));
-  ms = random(500, 700);
+  ms = random(RAND_DELAY_MIN, RAND_DELAY_MAX);
 
   sensor_state = digitalRead(sensor);
   if (!sensor_state) {
-    start_tree(ms);
+    result = start_tree(ms);
     if (result) {
       set_tree_state(OFF);
     }
@@ -59,19 +62,26 @@ void reset_tree(void)
 {
   register uint8_t i;
 
+  // flash tree NUM_FLASHES times then turn off.
   for(i = 0; i < NUM_FLASHES; i++) {
     set_tree_state(OFF);
     delay(250);
     set_tree_state(ON);
     delay(250);
   }
+  set_tree_state(OFF);
 }
 
-uint8_t start_tree(uint16_t ms)
+int8_t start_tree(uint16_t ms)
 {
   uint32_t start_time = millis(), leave_time;
-  uint16_t react;
+  int32_t react;
+  
 
+  /*
+      turn led_s on while sensor is covered and start sequence if sensor
+      remains covered for STAGE_TIME milliseconds.
+  */
   while (millis() - start_time < STAGE_TIME) {
     digitalWrite(s, OFF);
     if (!digitalRead(sensor)) {
@@ -83,27 +93,33 @@ uint8_t start_tree(uint16_t ms)
   
   delay(500);
   digitalWrite(y, ON);
-  delay(ms);
-  start_time = millis();
 
+  /*
+      after a delay set by caller, turn off led_s and led_y then check if 
+      obstacle is still present then turn on led_g, else turn on led_r.
+  */
+  delay(ms);
   digitalWrite(s, OFF);
   digitalWrite(y, OFF);
   if (digitalRead(sensor)) {
-    leave_time = millis();
     digitalWrite(r, ON);
+    react = -1;
   } else {
     digitalWrite(g, ON);
+    // mesaure reaction time in ms up until MAX_REACT.
+    start_time = millis();
     while (millis() - start_time < MAX_REACT) {
       if (digitalRead(sensor)) {
         break;
       }
     }
     leave_time = millis();
+    react = leave_time - start_time;
   }  
 
-  react = leave_time - start_time;
+  // print reaction to serial.
   Serial.print("\nReaction time: ");
-  if (react > 1 && react < MAX_REACT) {
+  if (react >= 0 && react < MAX_REACT) {
     Serial.print(react);
     Serial.println(" ms.");
   } else if (react >= MAX_REACT) {
