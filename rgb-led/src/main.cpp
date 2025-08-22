5d339638beeff4f3d2d0afd70937f35cce5c19bc
carlyle-felix <carlyle.felix@gmail.com>
2025-08-22 20:20:42 +0200

    rgb-led: night light

--- rgb-led/src/main.cpp
+++ rgb-led/src/main.cpp
@@ -1,26 +1,24 @@
 #include <Arduino.h>
 
-void set_colour(const uint8_t r, const uint8_t g, const uint8_t b);
+void set_colour(const int8_t r, const int8_t g, const int8_t b);
 
-const int8_t led = 13;
+const int8_t ph = A0;
 const int8_t red = 3;
 const int8_t green = 6;
 const int8_t blue = 9;
-const int8_t button = 2;
 
 void setup() {
-  pinMode(led, OUTPUT);
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
-  pinMode(button, INPUT);
 }
 
 void loop() {
+
   set_colour(0, 0, 0);
-  digitalWrite(led, HIGH);
-  while (digitalRead(button)) {
-    digitalWrite(led, LOW);
+  //digitalWrite(led, HIGH);
+  if (analogRead(ph) < 5) {
+    //digitalWrite(led, LOW);
     set_colour(255, 20, 147);
     delay(1000);
     set_colour(173, 216, 230);
@@ -30,7 +28,7 @@ void loop() {
   }
 }
 
-void set_colour(const uint8_t r, const uint8_t g, const uint8_t b)
+void set_colour(const int8_t r, const int8_t g, const int8_t b)
 {
   analogWrite(red, r);
   analogWrite(green, g);
